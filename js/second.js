940f42161510fb1031b069fbb7ec5c38f69bad6d
eternal <31749960+qhy354985240@users.noreply.github.com>
2018-05-05 09:08:38 +0800

    Update second.js

--- js/second.js
+++ js/second.js
@@ -4,12 +4,12 @@ function change(){
     var spantwo=document.getElementsByClassName("spantwo")[0];
     var tag=btn.getAttribute("data-checked");
     if(tag==="false"){
-        spantwo.style="transform: scaleX(1);";
+        spantwo.style="transform: scaleX(1); -webkit-transform: scaleX(1); -moz-transform: scaleX(1); -o--transform: scaleX(1);";
         span.style="color:#1ca1ef;";
         btn.setAttribute("data-checked","true");
     }else {
         btn.setAttribute("data-checked","false");
-        spantwo.style="transform: scaleX(0);";
+        spantwo.style="transform: scaleX(0); -webkit-transform: scaleX(0); -moz-transform: scaleX(0); -o--transform: scaleX(0);";
         span.style="color:#000;";
     }
 }
